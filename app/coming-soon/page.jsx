b24d8ba4336c0b058ac826fb7473b51bbb65e632
Mobina Alomohammadi <mobinaalimohammadi40@gmail.com>
2025-08-28 23:08:38 +0300

    feat: implement coming-soon page

---
+++ app/coming-soon/page.jsx
@@ -0,0 +1,17 @@
+/* coming soon component shows a message when user goes to incomplete
+parts of website */
+export default function ComingSoonPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: 24,
+      }}
+    >
+      🚧 This section is coming soon!
+    </div>
+  );
+}
